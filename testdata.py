dcb2a1a6367284cdaee438e8dbd226e63c1bbcf3
Juidas <143441437+Juidas@users.noreply.github.com>
2025-07-23 21:36:27 +0300

    Delete testdata.py

--- testdata.py
+++
@@ -1,8 +0,0 @@
-from app import db
-from app.models import Product
-
-p1 = Product(name="Laptop", description="Dell Latitude E5500", price=999.99)
-p2 = Product(name="Smartphone", description="Samsung Galaxy S24", price=799.99)
-
-db.session.add_all([p1, p2])
-db.session.commit()

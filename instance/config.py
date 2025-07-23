53f386801f77e6f5c852a86fc523a7d1560742c0
Juidas <143441437+Juidas@users.noreply.github.com>
2025-07-23 21:35:26 +0300

    Delete instance directory

--- instance/config.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY = 'FC!w^rnu#G8H$EQ-Ug$<B#xm_46tU'
-SQLALCHEMY_DATABASE_URI = 'postgresql://traditional_user:mUKtQW6r#HY$-J7^en#9z@localhost/traditional_db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SESSION_TYPE = 'filesystem'
